2e32d16fb86b1961d9469b25d84c51bebb31c4df
mdeanjones <mdeanjones@gmail.com>
2022-08-11 08:24:02 -0400

    feat: basic location checkin script to record lat/lng

---
+++ scripts/location-checkin.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+const fs   = require('fs');
+const path = require('path');
+
+const [lat, lng]   = process.argv.slice(2);
+
+if (!(lat && lng)) {
+  throw new Error('latitude and longitude are required');
+}
+
+const projectRoot  = path.normalize(path.join(__dirname, '..'));
+const checkinsPath = path.join(projectRoot, '_data', 'location-checkin.json');
+const checkins     = JSON.parse(fs.readFileSync(checkinsPath, 'utf8'));
+
+checkins.push({ lat, lng });
+
+fs.writeFileSync(checkinsPath, JSON.stringify(checkins), 'utf8');
