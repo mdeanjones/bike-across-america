31da9ee6f2fe82ecfbec724fbe2a561035e08710
mdeanjones <mdeanjones@gmail.com>
2022-08-10 21:22:09 -0400

    feat: cont tweaking side-scroller timings

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -49,25 +49,28 @@ class Countdown {
 
 class SideScroller {
   static clouds = [
-    { className: 'cloud', scaleBounds: [0.05, 0.25], flippable: true },
-  ];
-
-  static trees = [
-    { className: 'pine-tree', scaleBounds: [0.2, 0.5], flippable: true },
-    { className: 'oak-tree',  scaleBounds: [0.2, 0.5],  flippable: true },
-  ];
-
-  static foliage = [
-    { className: 'cactus',       flippable: true },
-    { className: 'tall-shrub',   flippable: true },
-    { className: 'chubby-shrub', flippable: true },
-    { className: 'grass-patch',  flippable: true },
-    { className: 'tumbleweed',   flippable: true, spin: true, accelerate: true },
+    { className: 'cloud', scaleBounds: [0.05, 0.3], flippable: true, verticalJitter: 200 },
   ];
 
   static terrain = [
-    { className: 'hills',     scaleBounds: [0.2, 0.5], flippable: true },
-    { className: 'mountains', scaleBounds: [0.2, 0.5], flippable: true },
+    { className: 'hills',     scaleBounds: [0.2, 0.5],  flippable: true },
+    { className: 'mountains', scaleBounds: [0.3, 0.65], flippable: true },
+  ];
+
+  static background = [
+    { className: 'pine-tree',    scaleBounds: [0.2, 0.5], flippable: true },
+    { className: 'oak-tree',     scaleBounds: [0.2, 0.5], flippable: true },
+    { className: 'cactus',       scaleBounds: [0.2, 0.5], flippable: true },
+    { className: 'tall-shrub',   scaleBounds: [0.2, 0.5], flippable: true },
+    { className: 'chubby-shrub', scaleBounds: [0.2, 0.5], flippable: true },
+  ];
+
+  static foreground = [
+    { className: 'cactus',       scaleBounds: [0.2, 0.5], flippable: true },
+    { className: 'tall-shrub',   scaleBounds: [0.2, 0.5], flippable: true },
+    { className: 'chubby-shrub', scaleBounds: [0.2, 0.5], flippable: true },
+    { className: 'grass-patch',  scaleBounds: [0.2, 0.5], flippable: true },
+    { className: 'tumbleweed',   scaleBounds: [0.2, 0.4], flippable: true, spin: true, accelerate: true },
   ];
 
   constructor() {
@@ -81,54 +84,35 @@ class SideScroller {
       event.target.parentNode.removeChild(event.target);
     });
 
-    this.insertElementFromCollection(SideScroller.clouds,  this.skyLayers,        [10000, 22000]);
-    this.insertElementFromCollection(SideScroller.terrain, this.terrainLayers,    [40000, 60000]);
-
-    this.insertElementFromCollection(
-      [...SideScroller.trees, ...SideScroller.foliage],
-      this.backgroundLayers,
-      [10000, 15000],
-    );
-
-    this.insertElementFromCollection(
-      SideScroller.foliage,
-      this.foregroundLayers,
-      [10000, 15000],
-    );
+    this.insertElementFromCollection(SideScroller.clouds,     this.skyLayers,        [3000, 9000],   [30000, 33000]);
+    this.insertElementFromCollection(SideScroller.terrain,    this.terrainLayers,    [15000, 30000], [40000, 60000]);
+    this.insertElementFromCollection(SideScroller.background, this.backgroundLayers, [10000, 15000], [25000, 35000]);
+    this.insertElementFromCollection(SideScroller.foreground, this.foregroundLayers, [80000, 13000], [18000, 27000]);
   }
 
-  insertElementFromCollection(collection, layerGroup, spawnRate) {
+  insertElementFromCollection(collection, layerGroup, spawnRate, scrollRate) {
     const layerIdx       = this.randomInteger(0, layerGroup.length - 1);
     const element        = this.createRandomElement(collection);
     const nextSpawnMs    = this.randomInteger(spawnRate[0], spawnRate[1]);
-    const durationJitter = this.randomInteger(0, nextSpawnMs * 0.1);
+    const scrollMs       = this.randomInteger(scrollRate[0], scrollRate[1]);
 
-    element.style.animationDuration = `${ nextSpawnMs + durationJitter - (layerIdx * durationJitter) }ms`;
+
+
+    element.style.animationDuration = `${ scrollMs - (layerIdx * scrollMs * 0.1) }ms`;
 
     layerGroup[layerIdx].appendChild(element);
 
     setTimeout(
-      () => this.insertElementFromCollection(collection, layerGroup, spawnRate),
+      () => this.insertElementFromCollection(collection, layerGroup, spawnRate, scrollRate),
       nextSpawnMs,
     );
   }
-
-  // insertTerrain() {
-  //   const layerIdx = this.randomInteger(0, this.terrainLayers.length - 1);
-  //   const element  = this.createRandomElement(SideScroller.terrain);
-  //
-  //   element.style.animationDuration = `${ 100 - (layerIdx * 20) }s`;
-  //
-  //   this.terrainLayers[layerIdx].appendChild(element);
-  //
-  //
-  // }
 
   createRandomElement(array) {
     return this.createElement(this.randomElement(array));
   }
 
-  createElement({ className, scaleBounds, flippable }) {
+  createElement({ className, scaleBounds, flippable, verticalJitter }) {
     const el = document.createElement('div');
     el.classList.add('element', className);
 
@@ -142,6 +126,10 @@ class SideScroller {
 
     if (flippable && this.randomInteger(0, 1) === 1) {
       el.style.transform += ' scaleX(-1)';
+    }
+
+    if (verticalJitter) {
+      el.style.transform += ` translateY(${ this.randomInteger(verticalJitter * -1, verticalJitter) }px)`;
     }
 
     return el;
