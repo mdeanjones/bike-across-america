eda5078a290fb973c1e9da6e74e901f40a4a198b
mdeanjones <mdeanjones@gmail.com>
2022-08-12 08:29:47 -0400

    map enhancements

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,10 +1,44 @@
 function initMap() {
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom:   14,
-    center: window.locationPins[0],
-    mapTypeId: 'terrain',
-  });
+  const mapEl   = document.getElementById("map");
+  const pins    = window.locationPins;
+  const iconUrl = 'http://maps.google.com/mapfiles/kml/shapes/cycling.png';
 
-  const bikeLayer = new google.maps.BicyclingLayer();
-  bikeLayer.setMap(map);
+  const map    = new google.maps.Map(mapEl, { mapTypeId: 'terrain' });
+  const bounds = new google.maps.LatLngBounds();
+  const info   = new google.maps.InfoWindow();
+
+  new google.maps.BicyclingLayer().setMap(map);
+
+  for (let pin of pins) {
+    bounds.extend(pin);
+  }
+
+  map.fitBounds(bounds);
+
+  // Cause the markers to drop in a staggered wave
+  function generateNextMarker(pins) {
+    const pin = pins.shift();
+
+    const marker = new google.maps.Marker({
+      map,
+      position: pin,
+      title: 'Hello World',
+      animation: google.maps.Animation.DROP,
+      icon: {
+        url: iconUrl,
+        scaledSize: new google.maps.Size(40, 40),
+      },
+    });
+
+    marker.addListener('click', () => {
+      info.setContent(`${ pin.lat }/${ pin.lng }`);
+      info.open({ map, anchor: marker });
+    });
+
+    if (pins.length) {
+      setTimeout(() => generateNextMarker(pins), 350);
+    }
+  }
+
+  generateNextMarker(pins.slice());
 }
