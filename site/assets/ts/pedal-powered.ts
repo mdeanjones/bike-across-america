63218e2085c6880adace473ef9104750dc7588ae
mdeanjones <mdeanjones@gmail.com>
2022-08-16 20:04:52 -0400

    more reorganization

--- site/assets/ts/pedal-powered.ts
+++
@@ -1,8 +0,0 @@
-import Countdown from './lib/countdown';
-import SideScroller from './lib/side-scroller';
-import setupMaps from './lib/map';
-
-new Countdown('2022-08-31T09:00:00-08:00', '#countdown');
-new SideScroller();
-
-setupMaps();
