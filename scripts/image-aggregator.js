2b499ec173e66b3d4f8dabbdefee98c9ed0cba9d
mdeanjones <mdeanjones@gmail.com>
2022-08-19 08:56:49 -0400

    fixup image-aggregator to better manage figuring out "new" photos

--- scripts/image-aggregator.js
+++ scripts/image-aggregator.js
@@ -16,75 +16,128 @@ function getFlickrUrl(apiKey, userId, photoSetId) {
   return url + `&api_key=${apiKey}&user_id=${userId}&extras=url_o,url_n,date_upload,geo&format=json&nojsoncallback=1`;
 }
 
-function getImageSubset({ ownername, photo }, tailId) {
-  const results    = [];
-  const imageArray = consistentSort(photo);
+/**
+ * @typedef {Object} FlickrPhoto
+ *
+ * @property {string} id
+ * @property {string} title
+ * @property {string} latitude
+ * @property {string} longitude
+ * @property {string} url_n
+ * @property {string} url_o
+ * @property {number} dateupload
+ * @property {number} height_n
+ * @property {number} width_n
+ * @property {number} height_o
+ * @property {number} width_o
+ */
 
-  for (let i = 0; i < imageArray.length; i += 1) {
-    const item = imageArray[i];
+/**
+ * @typedef {Object} GalleryPhoto
+ *
+ * @property {string} id
+ * @property {string} title
+ * @property {string} lat
+ * @property {string} lng
+ * @property {string} thumb
+ * @property {string} image
+ * @property {number} date
+ * @property {{ h: number, w: number }} txy
+ * @property {{ h: number, w: number }} ixy
+ */
 
-    if (item.id === tailId) {
-      break;
-    }
+/**
+ * @typedef {Object} LatestRecord
+ *
+ * @property {string} userId
+ * @property {string} idTimestamp
+ */
 
-    results.push({
-      id: item.id,
-      owner: ownername,
-      title: item.title,
-      lat: item.latitude,
-      lng: item.longitude,
-      date: item.dateupload,
-      thumb: item.url_n.replace('https://live.staticflickr.com/', ''),
-      image: item.url_o.replace('https://live.staticflickr.com/', ''),
-      txy: { h: item.height_n, w: item.width_n },
-      ixy: { h: item.height_o, w: item.width_o },
+/**
+ * @typedef {Object} Gallery
+ *
+ * @property {LatestRecord[]} latest
+ * @property {GalleryPhoto[]} images
+ */
+
+/**
+ * @param {Gallery} gallery
+ * @param {string} apiKey
+ * @param {string} userId
+ * @param {string} [photoSetId]
+ *
+ * @returns {Promise<Gallery>}
+ */
+async function loadNewImages(gallery, apiKey, userId, photoSetId) {
+  const url  = getFlickrUrl(apiKey, userId, photoSetId);
+  const json = await fetch(url).then(response => response.json());
+
+  /** @type {FlickrPhoto[]} */
+  const images = json.photoset.photo || json.photos.photo;
+
+  // Pull the unique ID of the last recorded "newest" image for the user
+  let latest = gallery.latest?.find(item => item.userId === userId);
+
+  // Find the index of the last recorded "newest" image within the new list
+  const endAt = latest?.idTimestamp
+    ? images.findIndex((item) => `${ item.id }-${ item.dateupload }` === latest.idTimestamp)
+    : images.length;
+
+  // The ID of the new "newest" image will always be derived from the first
+  // object of the response. In this way, it is very easy to determine whether
+  // any new images have been added.
+  const newIdTimestamp = images.length ? `${ images[0].id }-${ images[0].dateupload }` : '';
+
+  if (!latest) {
+    latest = { userId, idTimestamp: '' };
+    gallery.latest.push(latest);
+  }
+
+  latest.idTimestamp = newIdTimestamp;
+
+  const newImages = [];
+
+  for (let i = 0; i < endAt; i += 1) {
+    newImages.push({
+      owner: userId,
+      id:    images[i].id,
+      title: images[i].title,
+      lat:   images[i].latitude,
+      lng:   images[i].longitude,
+      date:  images[i].dateupload,
+      thumb: images[i].url_n.replace('https://live.staticflickr.com/', ''),
+      image: images[i].url_o.replace('https://live.staticflickr.com/', ''),
+      txy:   { h: images[i].height_n, w: images[i].width_n },
+      ixy:   { h: images[i].height_o, w: images[i].width_o },
     });
   }
 
-  return results;
-}
+  gallery.images = [...newImages, ...gallery.images];
 
-async function loadNewImages(currentImages, apiKey, userId, photoSetId) {
-  const json = await fetch(
-    getFlickrUrl(apiKey, userId, photoSetId),
-  ).then(response => response.json());
-
-  const newImages = getImageSubset(json.photoset || json.photos, currentImages[0]?.id);
-
-  return [...newImages, ...currentImages];
-}
-
-function consistentSort(imageArray) {
-  return imageArray.sort((a, b) => {
-    const dateA = a.date || a.dateupload;
-    const dateB = b.date || b.dateupload;
-
-    return dateA === dateB
-      ? a.id.localeCompare(b.id)
-      : dateB - dateA;
-  });
+  return gallery;
 }
 
 (async () => {
-  const flickrApiKey  = process.env.API_KEY;
-  const projectRoot   = path.normalize(path.join(__dirname, '..'));
-  const filePath      = path.join(projectRoot, 'site', '_data', 'gallery-wall.json');
-  const currentImages = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+  const apiKey   = process.env.API_KEY;
+  const rootPath = path.normalize(path.join(__dirname, '..'));
+  const filePath = path.join(rootPath, 'site', '_data', 'gallery-wall.json');
 
   const photoSources = [
     { userId: '196280425@N04', photoSetId: '72177720301304392' }, // Mike
     { userId: '196280101@N05', photoSetId: '72177720301383100' }, // Julie
   ];
 
-  let updatedGallery = currentImages;
+  /** @type {Gallery} */
+  let gallery = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+  let count   = gallery.images.length;
 
   for (let { userId, photoSetId } of photoSources) {
-    updatedGallery = await loadNewImages(updatedGallery, flickrApiKey, userId, photoSetId);
+    gallery = await loadNewImages(gallery, apiKey, userId, photoSetId);
   }
 
-  updatedGallery = consistentSort(updatedGallery);
+  gallery.images = gallery.images.sort((a, b) => b.date - a.date);
 
-  fs.writeFileSync(filePath, JSON.stringify(updatedGallery), 'utf8');
+  fs.writeFileSync(filePath, JSON.stringify(gallery), 'utf8');
 
-  console.log(`Added ${updatedGallery.length - currentImages.length} new image(s).`);
+  console.log(`Added ${gallery.images.length - count} new image(s).`);
 })();
